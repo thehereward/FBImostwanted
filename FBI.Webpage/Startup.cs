056989f6ffca76b9583a4907c9aa414470c9c0c4
Kiran Emery <K.Emery@ucas.ac.uk>
2020-01-27 11:48:15 +0000

    Switched to C#

---
+++ FBI.Webpage/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FBI.Webpage.Startup))]
+namespace FBI.Webpage
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
