886cd7848ac6109d5ee22bc8ca28079fea770103
Kiran Emery <K.Emery@ucas.ac.uk>
2020-02-06 11:11:41 +0000

    Added basic code for working out how to use google api for autocomplete of address

--- FBI.DataAccess/apiHandler.cs
+++ FBI.DataAccess/apiHandler.cs
@@ -13,5 +13,22 @@ namespace FBI.DataAccess
             var json = new WebClient().DownloadString($"https://api.fbi.gov/@wanted?page={page}");
             return JsonConvert.DeserializeObject<Root>(json, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
         }
+
+        public Location googleapi(string postCode)
+        {
+            var json = new WebClient().DownloadString($"https://maps.googleapis.com/maps/api/place/autocomplete/json?input={postCode}&key=AIzaSyBLGsg8An7pmw36PXm6xHtLth700SOSk6Q");
+            return JsonConvert.DeserializeObject<Location>(json);
+        }
+    }
+
+    public class Prediction
+    {
+        public string description { get; set; }
+    }
+
+    public class Location
+    {
+        public List<Prediction> predictions { get; set; }
+
     }
 }
